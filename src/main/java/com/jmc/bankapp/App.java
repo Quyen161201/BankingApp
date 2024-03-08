10b021d754f9f2f7b90e88823f767d4567f0aeac
MY PC <khaquyen317@gmail.com>
2024-03-09 02:57:46 +0700

    update view login, menu main

--- src/main/java/com/jmc/bankapp/App.java
+++ src/main/java/com/jmc/bankapp/App.java
@@ -1,4 +1,18 @@
 package com.jmc.bankapp;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
 }
---
+++ src/main/java/com/jmc/bankapp/Controllers/Client/ClientMenuController.java
@@ -0,0 +1,11 @@
+package com.jmc.bankapp.Controllers.Client;
+
+import javafx.scene.control.Button;
+
+public class ClientMenuController {
+    public Button dashboard_btn;
+    public Button transaction_btn;
+    public Button account_btn;
+    public Button profile_btn;
+    public Button logout_btn;
+}
